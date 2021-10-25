a47b159d2813ab162b12e2c160f7bd4d6d6d8ae1
Akshay S Kurup <akshayskurup@gmail.com>
2021-10-25 13:25:32 +0530

    Added poster

--- src/component/Movies/Movies.js
+++ src/component/Movies/Movies.js
@@ -1,11 +1,70 @@
 import React from 'react'
 import '../Movies/Movies.css'
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function Movies() {
+    let settings = {
+        dots: true,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 8,
+        slidesToScroll: 5,
+        autoplay: true
+};
     return (
-        <div className='title'>
-            <h2>Hello</h2>
-        </div>
+        <Slider className='Slider-two' {...settings}>
+            <div className='poster'>
+                <img className='poster-images' src="https://m.media-amazon.com/images/M/MV5BZWNiOTc4NGItNGY4YS00ZGNkLThkOWEtMDE2ODcxODEwNjkwXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg" alt="" /> 
+            </div>
+            <div className='poster'>
+                <img className='poster-images' src="https://m.media-amazon.com/images/M/MV5BZWNiOTc4NGItNGY4YS00ZGNkLThkOWEtMDE2ODcxODEwNjkwXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg" alt="" /> 
+            </div>
+            <div className='poster'>
+                <img className='poster-images' src="https://m.media-amazon.com/images/M/MV5BZWNiOTc4NGItNGY4YS00ZGNkLThkOWEtMDE2ODcxODEwNjkwXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg" alt="" /> 
+            </div>
+            <div className='poster'>
+                <img className='poster-images' src="https://m.media-amazon.com/images/M/MV5BZWNiOTc4NGItNGY4YS00ZGNkLThkOWEtMDE2ODcxODEwNjkwXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg" alt="" /> 
+            </div>
+            <div className='poster'>
+                <img className='poster-images' src="https://m.media-amazon.com/images/M/MV5BZWNiOTc4NGItNGY4YS00ZGNkLThkOWEtMDE2ODcxODEwNjkwXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg" alt="" /> 
+            </div>
+            <div className='poster'>
+                <img className='poster-images' src="https://m.media-amazon.com/images/M/MV5BZWNiOTc4NGItNGY4YS00ZGNkLThkOWEtMDE2ODcxODEwNjkwXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg" alt="" /> 
+            </div>
+            <div className='poster'>
+                <img className='poster-images' src="https://m.media-amazon.com/images/M/MV5BZWNiOTc4NGItNGY4YS00ZGNkLThkOWEtMDE2ODcxODEwNjkwXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg" alt="" /> 
+            </div>
+            <div className='poster'>
+                <img className='poster-images' src="https://m.media-amazon.com/images/M/MV5BZWNiOTc4NGItNGY4YS00ZGNkLThkOWEtMDE2ODcxODEwNjkwXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg" alt="" />
+            </div>
+            <div className='poster'>
+                <img className='poster-images' src="https://d2e111jq13me73.cloudfront.net/sites/default/files/styles/product_image_aspect_switcher_170w/public/product-images/csm-tv/the-simpsons-poster.jpg?itok=g53MiMST" alt="" />
+            </div>
+            <div className='poster'>
+                <img className='poster-images' src="https://d2e111jq13me73.cloudfront.net/sites/default/files/styles/product_image_aspect_switcher_170w/public/product-images/csm-tv/the-simpsons-poster.jpg?itok=g53MiMST" alt="" />
+            </div>
+            <div className='poster'>
+                <img className='poster-images' src="https://d2e111jq13me73.cloudfront.net/sites/default/files/styles/product_image_aspect_switcher_170w/public/product-images/csm-tv/the-simpsons-poster.jpg?itok=g53MiMST" alt="" />
+            </div>
+            <div className='poster'>
+                <img className='poster-images' src="https://d2e111jq13me73.cloudfront.net/sites/default/files/styles/product_image_aspect_switcher_170w/public/product-images/csm-tv/the-simpsons-poster.jpg?itok=g53MiMST" alt="" />
+            </div>
+            <div className='poster'>
+                <img className='poster-images' src="https://d2e111jq13me73.cloudfront.net/sites/default/files/styles/product_image_aspect_switcher_170w/public/product-images/csm-tv/the-simpsons-poster.jpg?itok=g53MiMST" alt="" />
+            </div>
+            <div className='poster'>
+                <img className='poster-images' src="https://d2e111jq13me73.cloudfront.net/sites/default/files/styles/product_image_aspect_switcher_170w/public/product-images/csm-tv/the-simpsons-poster.jpg?itok=g53MiMST" alt="" />
+            </div>
+            <div className='poster'>
+                <img className='poster-images' src="https://d2e111jq13me73.cloudfront.net/sites/default/files/styles/product_image_aspect_switcher_170w/public/product-images/csm-tv/the-simpsons-poster.jpg?itok=g53MiMST" alt="" />
+            </div>
+            <div className='poster'>
+                <img className='poster-images' src="https://d2e111jq13me73.cloudfront.net/sites/default/files/styles/product_image_aspect_switcher_170w/public/product-images/csm-tv/the-simpsons-poster.jpg?itok=g53MiMST" alt="" />
+            </div>
+
+        </Slider>
     )
 }
 
