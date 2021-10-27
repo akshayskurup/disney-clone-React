721ab70db75a9dffed59b013f0409c822c313221
Akshay S Kurup <akshayskurup@gmail.com>
2021-10-27 21:20:18 +0530

    Login page completed

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,14 +1,35 @@
-import React from 'react'
-import '../Pages/Login.css'
+import React from "react";
+import "../Pages/Login.css";
 function Login() {
-    return (
-        <div >
-            <div className='login-background'>
-                <img className='login-background' src="\Disney Plus\images\login-background.jpg" alt="" />
-                
-            </div>
+  return (
+    <div className="login-background">
+      <div className="login-background">
+        <img
+          className="login-background"
+          src="\Disney Plus\images\login-background.jpg"
+          alt=""
+        />
+      </div>
+      <div className="cta-logos">
+        <img
+          className="cta-logo-one"
+          src="\Disney Plus\images\cta-logo-one.svg"
+          alt=""
+        />
+        <div className="signup">GET ALL THERE</div>
+        <div className="signup-description">
+            <p className="signup-description">Get Premier Access to Raya and the Last Dragon for an additional fee with a Disney+ subscription. As of 03/26/21, the price of Disney+ and The Disney Bundle will increase by $1.</p>
         </div>
-    )
+        <div className="cta-logo-two">
+        <img
+          className="cta-logo-two"
+          src="\Disney Plus\images\cta-logo-two.png"
+          alt=""
+        />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
